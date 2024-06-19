1de3a655686a58bc96e8785c6186b6f68d78cc63
Yasmeen <yomalbalooshi@gmail.com>
2024-06-19 11:41:12 +0300

    app initialization

---
+++ GMTB-React-App/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import 'primereact/resources/themes/bootstrap4-light-blue/theme.css'
+const App = () => {
+  return (
+    <main className="font-nunito">
+      <p>page</p>
+    </main>
+  )
+}
+
+export default App
