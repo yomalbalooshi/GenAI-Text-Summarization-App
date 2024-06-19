006800292b42328030d80320b45d1f94faa23914
Yasmeen <yomalbalooshi@gmail.com>
2024-06-19 11:42:48 +0300

    header

---
+++ GMTB-React-App/src/components/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header className=" bg-blue  border-b border-light-grey">
+      <div className="px-4 mx-auto py-4 w-full sm:px-6 lg:px-8">
+        <div className="flex-shrink-0">
+          <span href="#" title="" className="flex justify-start">
+            <img className="w-auto h-8 lg:h-10" src="/appLogo.png" alt="" />
+            <h3 className=" text-white text-3xl font-semibold pl-3">
+              Give me the butter
+            </h3>
+          </span>
+        </div>
+      </div>
+    </header>
+  )
+}
+export default Header
